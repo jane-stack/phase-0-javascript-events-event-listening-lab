3e3c006afea3a483c8d9274a49ce3da6b1445670
Tuyen Ha <tuyen_ha@outlook.com>
2022-07-07 14:52:22 -0600

    commit tree 0b1272d2a51e0880e942effb4ebd5c1e63832d7a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('input');
+
+// input.addEventListener('click', addingEventListener);
+
 function addingEventListener() {
+    input.addEventListener('click', (e) => {
+        alert('I was clicked!')
+    })
 }
+
+addingEventListener();
